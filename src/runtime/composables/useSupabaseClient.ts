8c44a01019c8152f8b96cd07b9a5a5faf9c9fa47
Baptiste Leproux <leproux.baptiste@gmail.com>
2022-03-14 10:45:57 +0100

    fix(client): do not store var globally

--- src/runtime/composables/useSupabaseClient.ts
+++ src/runtime/composables/useSupabaseClient.ts
@@ -3,22 +3,20 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js'
 import { useRuntimeConfig, useNuxtApp } from '#app'
 import { useSupabaseToken } from './useSupabaseToken'
 
-let supabase: SupabaseClient
-
 export const useSupabaseClient = (): SupabaseClient => {
   const nuxtApp = useNuxtApp()
   const token = useSupabaseToken()
   const { supabase: { url, key, options } } = useRuntimeConfig()
 
   // No need to recreate client if exists
-  if (!supabase) {
-    supabase = createClient(url, key, options)
+  if (!nuxtApp._supabaseClient) {
+    nuxtApp._supabaseClient = createClient(url, key, options)
 
     //  Inject user's access_token in supabase client to make use of RLS on the server side
     if (nuxtApp.ssrContext) {
-      supabase.auth.setAuth(token.value)
+      nuxtApp._supabaseClient.auth.setAuth(token.value)
     }
   }
 
-  return supabase
+  return nuxtApp._supabaseClient
 }
