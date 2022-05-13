696d3e8d19b8db79c85e86b79a3ba4dd31bb2343
Baptiste Leproux <leproux.baptiste@gmail.com>
2022-05-13 16:30:05 +0200

    feat(server): add server routes services (#36)
    * feat(server): allow client usage on server routes
    
    * fix(module): alias typo
    
    * fix(ts): update
    
    * fix(server): return null user to avoid 404
    
    * feat(playground): update
    
    * feat(playground): client side call
    
    * docs(usage): add server routes services

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-    Supabase module playground!
+    <h1>
+      Supabase module playground!
+    </h1>
+    <button @click="fetchMe">
+      Fetch me from server route !
+    </button>
+    <pre>
+      {{ user }}
+    </pre>
   </div>
 </template>
 
 <script setup>
+const user = ref(null)
+
+const fetchMe = async () => {
+  user.value = await useFetch('/api/me', { headers: useRequestHeaders(['cookie']) })
+}
 </script>
---
+++ src/runtime/server/services/serverSupabaseUser.ts
@@ -0,0 +1,17 @@
+import { User } from '@supabase/supabase-js'
+import type { CompatibilityEvent } from 'h3'
+import { serverSupabaseClient } from '../services/serverSupabaseClient'
+
+export const serverSupabaseUser = async (event: CompatibilityEvent): Promise<User> => {
+  const client = serverSupabaseClient(event)
+
+  if (!event.context._token) {
+    return null
+  }
+
+  const { user: supabaseUser, error } = await client.auth.api.getUser(event.context._token)
+
+  event.context._user = error ? null : supabaseUser
+
+  return event.context._user
+}
