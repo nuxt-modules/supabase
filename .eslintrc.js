c24a16ff0bc94221adc021231a2206b6db8f293c
Baptiste Leproux <leproux.baptiste@gmail.com>
2022-06-01 09:32:12 +0200

    refactor(docs): use nuxt content with docus theme (#41)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@ module.exports = {
   root: true,
   extends: [
     '@nuxtjs/eslint-config-typescript'
-  ]
+  ],
+  rules: {
+    'vue/multi-word-component-names': ['error', {
+      ignores: ['Logo']
+    }]
+  }
 }
