15b02177b13b1e93cc9b697a4ba3494719b031ec
Baptiste Leproux <leproux.baptiste@gmail.com>
2022-08-17 19:33:32 +0200

    fix(client): recreate client when token has changed

--- src/runtime/composables/useSupabaseClient.ts
+++ src/runtime/composables/useSupabaseClient.ts
@@ -6,14 +6,18 @@ import { useRuntimeConfig, useNuxtApp } from '#imports'
 export const useSupabaseClient = (): SupabaseClient => {
   const nuxtApp = useNuxtApp()
   const token = useSupabaseToken()
+  const Authorization = token.value ? `Bearer ${token.value}` : undefined
 
   const { supabase: { url, key, client: clientOptions } } = useRuntimeConfig().public
 
   // Set auth header to make use of RLS
-  const options = defu(clientOptions, { global: { headers: { Authorization: `Bearer ${token.value}` } } })
+  const options = defu(clientOptions, { global: { headers: { Authorization } } })
+
+  // Recreate client if token has changed
+  const recreateClient = nuxtApp._supabaseClient?.headers.Authorization !== Authorization
 
   // No need to recreate client if exists
-  if (!nuxtApp._supabaseClient) {
+  if (!nuxtApp._supabaseClient || recreateClient) {
     nuxtApp._supabaseClient = createClient(url, key, options)
   }
 
