bce6ccdd9e8b47ecff30f8bbcc8fcf3feada0d8a
Baptiste Leproux <leproux.baptiste@gmail.com>
2022-08-17 18:24:53 +0200

    fix(ssr): set token when initializaing client

--- src/runtime/composables/useSupabaseClient.ts
+++ src/runtime/composables/useSupabaseClient.ts
@@ -1,20 +1,20 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
+import { defu } from 'defu'
 import { useSupabaseToken } from './useSupabaseToken'
 import { useRuntimeConfig, useNuxtApp } from '#imports'
 
 export const useSupabaseClient = (): SupabaseClient => {
   const nuxtApp = useNuxtApp()
   const token = useSupabaseToken()
-  const { supabase: { url, key, client: options } } = useRuntimeConfig().public
+
+  const { supabase: { url, key, client: clientOptions } } = useRuntimeConfig().public
+
+  // Set auth header to make use of RLS
+  const options = defu(clientOptions, { global: { headers: { Authorization: `Bearer ${token.value}` } } })
 
   // No need to recreate client if exists
   if (!nuxtApp._supabaseClient) {
     nuxtApp._supabaseClient = createClient(url, key, options)
-
-    //  Inject user's access_token in supabase client to make use of RLS on the server side
-    if (nuxtApp.ssrContext) {
-      nuxtApp._supabaseClient.auth.setSession(token.value)
-    }
   }
 
   return nuxtApp._supabaseClient
