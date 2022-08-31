8ff659cdf1322bdd42bb97038af2702fe62c7901
Baptiste Leproux <leproux.baptiste@gmail.com>
2022-08-31 16:15:43 +0200

    fix(plugins): imports useRoute

--- src/runtime/plugins/supabase.server.ts
+++ src/runtime/plugins/supabase.server.ts
@@ -1,7 +1,7 @@
 import { useSupabaseUser } from '../composables/useSupabaseUser'
 import { useSupabaseClient } from '../composables/useSupabaseClient'
 import { useSupabaseToken } from '../composables/useSupabaseToken'
-import { defineNuxtPlugin, useRuntimeConfig } from '#imports'
+import { defineNuxtPlugin, useRuntimeConfig, useRoute } from '#imports'
 
 // Set subabase user on server side
 export default defineNuxtPlugin(async () => {
