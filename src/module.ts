9338570332ca85e85f9f31d74d7726674ce25368
Daniel Roe <daniel@roe.dev>
2022-09-03 14:56:44 +0100

    fix: update nuxt compatibility

--- src/module.ts
+++ src/module.ts
@@ -59,7 +59,7 @@ export default defineNuxtModule<ModuleOptions>({
     name: '@nuxtjs/supabase',
     configKey: 'supabase',
     compatibility: {
-      nuxt: '^3.0.0'
+      nuxt: '^3.0.0-rc.8'
     }
   },
   defaults: {
