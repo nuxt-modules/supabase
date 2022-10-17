519e951a802af5d0c6e65b5744c450ad3e3ab24a
Baptiste Leproux <leproux.baptiste@gmail.com>
2022-10-17 15:44:36 +0200

    feat(session): handle refresh token

--- src/runtime/server/api/session.ts
+++ src/runtime/server/api/session.ts
@@ -14,29 +14,30 @@ export default defineEventHandler(async (event) => {
 
   if (signEvent === 'SIGNED_IN') {
     if (!session) { throw new Error('Auth session missing!') }
-    setCookie(
-      event,
-      `${cookieOptions.name}-access-token`,
-      session.access_token,
-      {
-        domain: cookieOptions.domain,
-        maxAge: cookieOptions.lifetime ?? 0,
-        path: cookieOptions.path,
-        sameSite: cookieOptions.sameSite as boolean | 'lax' | 'strict' | 'none'
-      }
+    setCookie(event, `${cookieOptions.name}-access-token`, session.access_token, {
+      domain: cookieOptions.domain,
+      maxAge: cookieOptions.lifetime ?? 0,
+      path: cookieOptions.path,
+      sameSite: cookieOptions.sameSite as boolean | 'lax' | 'strict' | 'none'
+    }
     )
+    setCookie(event, `${cookieOptions.name}-refresh-token`, session.refresh_token, {
+      domain: cookieOptions.domain,
+      maxAge: cookieOptions.lifetime ?? 0,
+      path: cookieOptions.path,
+      sameSite: cookieOptions.sameSite as boolean | 'lax' | 'strict' | 'none'
+    })
   }
 
   if (signEvent === 'SIGNED_OUT') {
-    setCookie(
-      event,
-      `${cookieOptions.name}-access-token`,
-      '',
-      {
-        maxAge: -1,
-        path: cookieOptions.path
-      }
-    )
+    setCookie(event, `${cookieOptions.name}-access-token`, '', {
+      maxAge: -1,
+      path: cookieOptions.path
+    })
+    setCookie(event, `${cookieOptions.name}-refresh-token`, '', {
+      maxAge: -1,
+      path: cookieOptions.path
+    })
   }
 
   return 'auth cookie set'
