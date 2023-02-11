018f018b615697458d61b5b721eb280d04a9f102
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-02-11 16:43:20 +0100

    fix(lint): remove unecesseray comma

--- docs-new/app.config.ts
+++ docs-new/app.config.ts
@@ -5,7 +5,7 @@ export default defineAppConfig({
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxt_themes',
-      github: 'nuxt-themes/docus',
+      github: 'nuxt-themes/docus'
     },
     aside: {
       level: 0,
