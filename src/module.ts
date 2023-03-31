b206fdde8e6178bd28abd2a8e1921eaf346b4489
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-04-01 00:06:36 +0200

    chore(module): remove websocket transpile

--- src/module.ts
+++ src/module.ts
@@ -172,22 +172,22 @@ export default defineNuxtModule<ModuleOptions>({
       options.references.push({ path: resolve(nuxt.options.buildDir, 'types/supabase.d.ts') })
     })
 
-    // Optimize cross-fetch
+    // // Optimize cross-fetch
     extendViteConfig((config) => {
       config.optimizeDeps = config.optimizeDeps || {}
       config.optimizeDeps.include = config.optimizeDeps.include || []
       config.optimizeDeps.include.push('cross-fetch')
     })
 
-    if (nuxt.options.dev) {
-      extendViteConfig((config) => {
-        config.optimizeDeps = config.optimizeDeps || {}
-        config.optimizeDeps.include = config.optimizeDeps.include || []
-        config.optimizeDeps.include.push('websocket')
-      })
-      // Transpile websocket only for non dev environments
-    } else if (!['cloudflare'].includes(process.env.NITRO_PRESET as string)) {
-      nuxt.options.build.transpile.push('websocket')
-    }
+    // if (nuxt.options.dev) {
+    // extendViteConfig((config) => {
+    //   config.optimizeDeps = config.optimizeDeps || {}
+    //   config.optimizeDeps.include = config.optimizeDeps.include || []
+    //   config.optimizeDeps.include.push('websocket')
+    // })
+    // Transpile websocket only for non dev environments
+    // } else if (!['cloudflare'].includes(process.env.NITRO_PRESET as string)) {
+    //   nuxt.options.build.transpile.push('websocket')
+    // }
   }
 })
