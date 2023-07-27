09fa4f8299e2ef1ecd6ca7dca8094582559a3084
Aietes <stefan@standa.de>
2023-07-27 18:45:19 +0200

    Fix global auth middleware

--- src/runtime/plugins/auth-redirect.ts
+++ src/runtime/plugins/auth-redirect.ts
@@ -1,18 +1,21 @@
 import { useSupabaseUser } from '../composables/useSupabaseUser'
-import { defineNuxtPlugin, addRouteMiddleware, useRuntimeConfig, navigateTo } from '#imports'
+import { defineNuxtPlugin, addRouteMiddleware, defineNuxtRouteMiddleware, useRuntimeConfig, navigateTo } from '#imports'
 
-export default defineNuxtPlugin(() => {
-  addRouteMiddleware(
-    'global-auth',
-    async to => {
-      if (to.path === '/login' || to.path === '/confirm') return
-      const user = await useSupabaseUser()
-      if (!user && process.client) {
-        console.log('no user, redirecting to login')
-        const loginUrl = useRuntimeConfig().public.supabase.redirectOptions.login
-        return navigateTo(loginUrl)
-      }
-    },
-    { global: true },
-  )
+export default defineNuxtPlugin({
+  name: 'auth-redirect',
+  setup() {
+    addRouteMiddleware(
+      'global-auth',
+      defineNuxtRouteMiddleware(async to => {
+        if (to.path === '/login' || to.path === '/confirm') return
+        const user = await useSupabaseUser()
+        if (!user) {
+          console.log('no user, redirecting to login')
+          const loginUrl = useRuntimeConfig().public.supabase.redirectOptions.login
+          return navigateTo(loginUrl)
+        }
+      }),
+      { global: true },
+    )
+  },
 })
