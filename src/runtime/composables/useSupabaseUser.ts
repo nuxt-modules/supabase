87c57df7d3498cd053d0c5938a406b875d0b1c84
Aietes <stefan@standa.de>
2023-07-30 13:20:23 +0200

    Fix getting runtimeOptions

--- src/runtime/composables/useSupabaseUser.ts
+++ src/runtime/composables/useSupabaseUser.ts
@@ -1,5 +1,4 @@
 import { User } from '@supabase/supabase-js'
-import { CookieOptions } from '../types'
 import { useCookie, useRuntimeConfig } from '#imports'
 import { useSupabaseClient } from '../composables/useSupabaseClient'
 
@@ -8,13 +7,10 @@ export const useSupabaseUser = async (): Promise<User> => {
 
   // get cookie setting from runtime config
   const runtime = useRuntimeConfig()
-  const { name, sameSite, ...options } = runtime.public.supabase.cookieOptions
-  const cookieOptions: CookieOptions = {
-    ...options,
-    sameSite: sameSite as boolean | 'lax' | 'strict' | 'none',
-  }
-  const accessToken = useCookie(`${name}-access-token`, cookieOptions).value
-  const refreshToken = useCookie(`${name}-refresh-token`, cookieOptions).value
+  const { cookieName, cookieOptions } = runtime.public.supabase
+
+  const accessToken = useCookie(`${cookieName}-access-token`, cookieOptions).value
+  const refreshToken = useCookie(`${cookieName}-refresh-token`, cookieOptions).value
 
   // check for session on client and server respectively
   const result = await supabase.auth.getSession()
