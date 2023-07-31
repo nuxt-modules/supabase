57ac16203765362e62767d6d8802c03de9ec49d5
Aietes <stefan@standa.de>
2023-07-31 12:06:30 +0200

    Removed legacy types

--- src/runtime/types/index.d.ts
+++ src/runtime/types/index.d.ts
@@ -14,20 +14,6 @@ declare module '@nuxt/schema' {
   }
 }
 
-export type SupabaseAuthProvider =
-  | 'github'
-  | 'facebook'
-  | 'google'
-  | 'cognito'
-  | 'twitter'
-  | 'discord'
-  | 'twitch'
-  | 'instagram'
-  | 'vk'
-  | 'linkedin'
-  | 'reddit'
-  | 'auth0'
-
 export interface RedirectOptions {
   login: string
   callback: string
