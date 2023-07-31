f5adf69f30790af7748414e16000fb3d1010f73e
Aietes <stefan@standa.de>
2023-07-31 12:05:42 +0200

    Removed middleware from playground

--- playground/middleware/auth.global.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware(async to => {
-  // if (to.path === '/login' || to.path === '/confirm') return
-  // const user = await useSupabaseUser()
-  // if (!user) {
-  //   console.log('No Supabase user, redirecting to login')
-  //   return navigateTo('/login')
-  // }
-})
