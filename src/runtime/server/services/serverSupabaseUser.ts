b9f9076a29ae777937ffe8b16ca26f448c93cdac
Aietes <stefan@standa.de>
2023-08-03 11:15:51 +0200

    Adding import for createError

--- src/runtime/server/services/serverSupabaseUser.ts
+++ src/runtime/server/services/serverSupabaseUser.ts
@@ -1,11 +1,15 @@
 import { User } from '@supabase/supabase-js'
 import type { H3Event } from 'h3'
+import { createError } from 'h3'
 import { serverSupabaseClient } from '../services/serverSupabaseClient'
 
 export const serverSupabaseUser = async (event: H3Event): Promise<User | null> => {
   const client = await serverSupabaseClient(event)
 
-  const { data: { user: supabaseUser }, error } = await client.auth.getUser()
+  const {
+    data: { user: supabaseUser },
+    error,
+  } = await client.auth.getUser()
   if (error) {
     throw createError({ statusMessage: error.message })
   }
