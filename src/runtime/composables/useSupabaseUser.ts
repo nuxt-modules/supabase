278cbd43e9c64ca500e91f81997838561801479f
Alex Muench <ammuench@gmail.com>
2023-09-21 22:46:32 -0500

    🐛 Makes `useSupabaseUser` an async function
    * Labels `useSupabaseUser` as an async function to resolve
    * Rewrite logic to properly encapsulate the asynchronous logic within
      the composable with await functions intead

--- src/runtime/composables/useSupabaseUser.ts
+++ src/runtime/composables/useSupabaseUser.ts
@@ -1,21 +1,22 @@
 import type { User } from '@supabase/supabase-js'
 import { useState } from '#imports'
 
-export const useSupabaseUser = () => {
+export const useSupabaseUser = async () => {
   const supabase = useSupabaseClient()
 
   const user = useState<User | null>('supabase_user', () => null)
 
   // Asyncronous refresh session and ensure user is still logged in
-  supabase?.auth.getSession().then(({ data: { session } }) => {
-    if (session) {
-      if (JSON.stringify(user.value) !== JSON.stringify(session.user)) {
-        user.value = session.user;
-      }
-    } else {
-      user.value = null
+  const sessionData = await supabase?.auth.getSession()
+  const session = sessionData.data.session
+
+  if (session) {
+    if (JSON.stringify(user.value) !== JSON.stringify(session.user)) {
+      user.value = session.user
     }
-  })
+  } else {
+    user.value = null
+  }
 
   return user as Ref<User | null>
 }
