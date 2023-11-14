a3ba956597d410620a1655113860836bf82ce9ef
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-11-14 16:55:47 +0100

    chore(app): initial commit by Nuxt Studio

---
+++ docs-pro/nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxtjs/fontaine',
+    '@nuxtjs/google-fonts',
+    'nuxt-og-image'
+  ],
+  ui: {
+    icons: ['heroicons', 'simple-icons']
+  },
+  // Fonts
+  fontMetrics: {
+    fonts: ['DM Sans']
+  },
+  googleFonts: {
+    display: 'swap',
+    download: true,
+    families: {
+      'DM+Sans': [400, 500, 600, 700]
+    }
+  },
+  nitro: {
+    prerender: {
+      routes: [
+        '/api/search.json'
+      ]
+    }
+  },
+  // Essential for OgImage on `nuxt generate`
+  // https://github.com/harlan-zw/nuxt-og-image/blob/c89fd4e29f56eeb00b12bc0d7e4bcb82ab459cae/src/module.ts#L127C16-L127C16
+  site: {
+    url: 'https://nuxt-ui-pro-template-docs.vercel.app'
+  },
+  // Devtools / Typescript
+  devtools: { enabled: true },
+  typescript: { strict: false }
+})
---
+++ docs-pro/pages/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const { data: page } = await useAsyncData('index', () => queryContent('/').findOne())
+
+useSeoMeta({
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: page.value.description,
+  ogDescription: page.value.description
+})
+
+defineOgImage({
+  component: 'Docs',
+  title: page.value.title,
+  description: page.value.description
+})
+</script>
+
+<template>
+  <div>
+    <ULandingHero v-if="page.hero" v-bind="page.hero">
+      <template #title>
+        <MDC :value="page.hero.title" />
+      </template>
+
+      <MDC :value="page.hero.code" tag="pre" class="prose prose-primary dark:prose-invert max-w-none" />
+    </ULandingHero>
+
+    <ULandingSection :title="page.features.title">
+      <UPageGrid>
+        <ULandingCard v-for="(item, index) of page.features.items" :key="index" v-bind="item" />
+      </UPageGrid>
+    </ULandingSection>
+  </div>
+</template>
