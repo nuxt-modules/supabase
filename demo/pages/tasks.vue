28f0e400645b6ec7efcc56d96a6a7a1818b55ac1
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-11-17 15:04:03 +0100

    chore(demo): fix types

--- demo/pages/tasks.vue
+++ demo/pages/tasks.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Database } from '~~/types/database.types'
+import type { Database } from '~~/types/database.types'
 
 const client = useSupabaseClient<Database>()
 const user = useSupabaseUser()
