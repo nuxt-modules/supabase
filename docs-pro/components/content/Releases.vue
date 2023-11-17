0f0bb1b79fc277884c2484a0b4bc9bae72b0b025
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-11-17 16:17:14 +0100

    docs(new): ui pro docs

---
+++ docs-pro/components/content/Releases.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="margin-top: 50px">
+    <div v-for="release of releases" :key="release.name" style="margin-bottom: 50px">
+      <ProseH2 :id="release.name">
+        {{ release.name }}
+        <Badge style="margin-left: 10px;">
+          {{ formatDateByLocale(release.date) }}
+        </Badge>
+      </ProseH2>
+
+      <ContentRenderer :value="release" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { fetchReleases } = useGithub()
+const { data: releases } = await useAsyncData('releases', () => fetchReleases({}))
+</script>
