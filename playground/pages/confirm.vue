0caf5c11d632df6b7e74e5e8973ee56a38b1c968
Baptiste Leproux <leproux.baptiste@gmail.com>
2024-01-26 11:44:45 +0100

    fix(playgorund): watch on confirm page

--- playground/pages/confirm.vue
+++ playground/pages/confirm.vue
@@ -5,10 +5,13 @@ if (process.server) {
     'ERROR: It is very important to make sure that the redirect route right after login works without any server-side rendering. This is because the server-side rendering will not have the user data available. This is why we need to redirect to a client-side route right after login. This is a limitation of Supabase. If you are using Nuxt, you can use the no-ssr component to make sure that the redirect route is not server-side rendered.'
   )
 }
+
 const user = useSupabaseUser()
-if (user.value) {
-  await navigateTo('/user')
-}
+watch(user, () => {
+  if (user.value) {
+    return navigateTo('/user')
+  }
+}, { immediate: true })
 </script>
 <template>
   <div>Waiting for login...</div>
