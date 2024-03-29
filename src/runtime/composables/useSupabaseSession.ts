07eb081d532510da4152554c7010c79ed6008559
Joey Chilson <100753878+joeychilson@users.noreply.github.com>
2024-03-29 03:59:36 -0500

    feat(session): add support for retrieving user session (#338)

---
+++ src/runtime/composables/useSupabaseSession.ts
@@ -0,0 +1,23 @@
+import type { Session } from '@supabase/supabase-js'
+import type { Ref } from 'vue'
+import { useSupabaseClient } from './useSupabaseClient'
+import { useState } from '#imports'
+
+export const useSupabaseSession = () => {
+  const supabase = useSupabaseClient()
+
+  const sessionState = useState<Session | null>('supabase_session', () => null)
+
+  // Asyncronous refresh session and ensure user is still logged in
+  supabase?.auth.getSession().then(({ data: { session } }) => {
+    if (session) {
+      if (JSON.stringify(sessionState.value) !== JSON.stringify(session)) {
+        sessionState.value = session
+      }
+    } else {
+      sessionState.value = null
+    }
+  })
+
+  return sessionState as Ref<Session | null>
+}
