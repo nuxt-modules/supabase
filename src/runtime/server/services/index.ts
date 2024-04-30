7205ed4d698db63d6213ee6d9592b5dfa724b8ed
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-04-30 14:02:43 +0500

    Fix missing export for serverSupabaseSession
    Add missing export for server session which was added in: https://github.com/nuxt-modules/supabase/pull/338/files

--- src/runtime/server/services/index.ts
+++ src/runtime/server/services/index.ts
@@ -1,3 +1,4 @@
 export { serverSupabaseClient } from './serverSupabaseClient'
 export { serverSupabaseServiceRole } from './serverSupabaseServiceRole'
 export { serverSupabaseUser } from './serverSupabaseUser'
+export { serverSupabaseSession } from './serverSupabaseSession'
