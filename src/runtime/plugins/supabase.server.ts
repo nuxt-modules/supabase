95d905b9b1f54a3aa15b3e4e583151cfcf00878f
Thimo Sietsma <thimosietsma@gmail.com>
2024-10-11 09:25:28 +0200

    fix(session): ssr getSession warning (#418)

--- src/runtime/plugins/supabase.server.ts
+++ src/runtime/plugins/supabase.server.ts
@@ -1,6 +1,7 @@
 import { createServerClient, parseCookieHeader } from '@supabase/ssr'
 import { getHeader, setCookie } from 'h3'
 import { fetchWithRetry } from '../utils/fetch-retry'
+import { serverSupabaseUser, serverSupabaseSession } from '../server/services'
 import { defineNuxtPlugin, useRequestEvent, useRuntimeConfig, useSupabaseSession, useSupabaseUser } from '#imports'
 import type { CookieOptions } from '#app'
 
@@ -33,13 +34,13 @@ export default defineNuxtPlugin({
 
     // Initialize user and session states
     const [
-      {
-        data: { session },
-      },
-      {
-        data: { user },
-      },
-    ] = await Promise.all([client.auth.getSession(), client.auth.getUser()])
+      session,
+      user,
+    ] = await Promise.all([
+      serverSupabaseSession(event).catch(() => null),
+      serverSupabaseUser(event).catch(() => null),
+    ])
+
     useSupabaseSession().value = session
     useSupabaseUser().value = user
 
--- src/runtime/server/services/serverSupabaseSession.ts
+++ src/runtime/server/services/serverSupabaseSession.ts
@@ -3,7 +3,7 @@ import type { H3Event } from 'h3'
 import { createError } from 'h3'
 import { serverSupabaseClient } from '../services/serverSupabaseClient'
 
-export const serverSupabaseSession = async (event: H3Event): Promise<Session | null> => {
+export const serverSupabaseSession = async (event: H3Event): Promise<Omit<Session, 'user'> | null> => {
   const client = await serverSupabaseClient(event)
 
   const { data: { session }, error } = await client.auth.getSession()
@@ -11,5 +11,7 @@ export const serverSupabaseSession = async (event: H3Event): Promise<Session | n
     throw createError({ statusMessage: error?.message })
   }
 
+  // @ts-expect-error we need to delete user from the session object here to suppress the warning coming from GoTrueClient
+  delete session?.user
   return session
 }
