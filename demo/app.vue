65832ba113af0d5ea8035b42fc6b7748b39fec4d
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-05-22 10:21:42 +0200

    fix(demo): use head

--- demo/app.vue
+++ demo/app.vue
@@ -7,6 +7,18 @@
 </template>
 
 <script setup lang="ts">
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' },
+  ],
+  htmlAttrs: {
+    lang: 'en',
+  },
+})
+
 const title = 'Nuxt 3 x Supabase'
 const description = 'Demo of a simple todo list app using Nuxt and Supabase.'
 
